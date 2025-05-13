6e463cdd0eb1d3db5ece10ac2b185c049eb8b126
imperator <leobinbauer@gmail.com>
2025-05-13 15:50:17 +0200

    ...

--- src/Components/FallingSand.tsx
+++ src/Components/FallingSand.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { PickedColorContainer } from "./PickedColorContainer";
 
 export const FallingSand = React.memo(({ sidebar }: { sidebar: boolean }) => {
